e478784edc9bd64c3a5d8350bc774ec551388243
1vivekanand <1vivekanandbhardwaj@gmail.com>
2022-01-12 21:44:06 +0530

    Microsoft

--- Microsoft/Day 1/Rotate-by-90-degree.java
+++ Microsoft/Day 1/Rotate-by-90-degree.java
@@ -1,5 +1,7 @@
 Problem Link : https://practice.geeksforgeeks.org/problems/rotate-by-90-degree0356/1/#
 
+Approach 1 : Two pass
+
 class GFG
 {
     static void swap(int[][] matrix,int i,int j){
@@ -31,3 +33,26 @@ class GFG
         }
     }
 }
+
+Problem Link : https://leetcode.com/problems/rotate-image/
+
+Approach 2: Single pass
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+	// take 4 corner element and rotate them
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
+
+
